4afc5a55c3948f3068025297fc7d2b107d74f7b9
Krzysztof K <krzysztof.kolowrocki.96@gmail.com>
2023-08-20 13:44:09 +0200

    added basci if statements exercise

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,12 +1,14 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
-
 import { ReactEventsUseStateMetaData } from './ReactEventsUseState/router-data';
+import { ReactIfStatementsMetaData } from './ReactIfStatements/router-data';
+
 
 export const blockRouterMetaData = [
     SubRouteExampleMetaData,
     ReactEventsUseStateMetaData,
+    ReactIfStatementsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
 
