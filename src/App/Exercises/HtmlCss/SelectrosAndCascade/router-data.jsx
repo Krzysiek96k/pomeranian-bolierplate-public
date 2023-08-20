efbd456a41373390198f4b1a3c698b643d089479
Krzysztof K <krzysztof.kolowrocki.96@gmail.com>
2023-08-20 16:56:41 +0200

    added basic informations about cascades and inheritance

---
+++ src/App/Exercises/HtmlCss/SelectrosAndCascade/router-data.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'css-cascade-and-selectros',
+  date: '20-08-2023',
+  linkLabel: 'HTML & CSS - kaskadowść, dziedziczenie i selektory',
+  blockNo: 10,
+  element: <Exercise />,
+  tags: [
+    'css',
+    'gpseudoelements',
+    'pseudoclasses',
+    'selectors',
+    'inheritance',
+  ],
+};
