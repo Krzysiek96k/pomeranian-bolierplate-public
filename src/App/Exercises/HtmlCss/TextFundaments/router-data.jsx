39d7aeea5c4bace40cb3ba566f583b976d0a6f53
Krzysztof K <krzysztof.kolowrocki.96@gmail.com>
2023-08-27 15:55:01 +0200

    text

---
+++ src/App/Exercises/HtmlCss/TextFundaments/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'text-fundaments-css',
+  date: '12-12-2023',
+  linkLabel: 'Praca z textem',
+  blockNo: 1,
+  element: <Exercise />,
+  tags: ['attribute', 'text', 'css'],
+};
