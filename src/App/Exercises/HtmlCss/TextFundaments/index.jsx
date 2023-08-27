4836ab941ebaf71945552615b852d055ae823bb2
Krzysztof K <krzysztof.kolowrocki.96@gmail.com>
2023-08-27 16:35:07 +0200

    add

--- src/App/Exercises/HtmlCss/TextFundaments/index.jsx
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -1,5 +1,12 @@
 import './styles.css';
 
 export function Exercise() {
-  return <p>Ćwiczenia z HTML & CSS - 1</p>;
+  return (
+    <p className="test">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui suscipit
+      dolorum nesciunt reiciendis possimus repudiandae aperiam rem quos sunt,
+      magni asperiores laudantium ducimus iste velit atque nobis eos aliquid
+      nostrum!
+    </p>
+  );
 }
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaTextFundaments } from './TextFundaments/router-data';
 import { blockRouterMetaData as CSSCascadesBlockRouterMetaData } from './SelectrosAndCascade/router-data';
+import { blockRouterMetaData as blockRouterMetaStandardTags } from './StandardTags/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   CSSCascadesBlockRouterMetaData,
   blockRouterMetaTextFundaments,
+  blockRouterMetaStandardTags,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
